0d6862c295e704f9c4aa7f3b0278b2d9f72a74fb
Barbossnya <Barbossnya@hotmail.com>
2023-10-11 12:55:17 +0300

    2.6 задач было решено

--- Tasks/task13/Program.cs
+++ Tasks/task13/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
 
-task13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
@@ -27,59 +27,6 @@ if (number < 100)
 	
 
 
-/*
-
-мой собственный пример
-
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > 999 / 10)
-{
-	Console.WriteLine(number % 10);
-}
-	
-if (number < 100)
-{
-	Console.WriteLine("Третьей цифры нет");
-}
-
-гибриды
-
-System.Console.Write("Введите число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-int length = Number.ToString().Length;
-
-if (length < 3)
-{
-	System.Console.WriteLine("Число не содержит третьей цифры");
-}
-else
-{
-	int result = Number / 100 % 10;
-	System.Console.WriteLine(result);
-}
-
-
-
-System.Console.Write("Введите число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-
-if (Number > 999)
-{
-	int result = Number / 100 % 10;
-	System.Console.WriteLine(result);
-}
-else if (Number > 100)
-{
-	System.Console.WriteLine("Третьей цифры нет");
-}
-else
-{
-	System.Console.WriteLine("Число должно быть больше 100");
-}
-
-
-*/
 
 
 
