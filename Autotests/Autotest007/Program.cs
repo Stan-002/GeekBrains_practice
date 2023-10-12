47b2e69b4ed2d55e0db3de64f9071f0df091a86a
Barbossnya <Barbossnya@hotmail.com>
2023-10-12 13:21:17 +0300

    Add change folder Autotests

--- Autotests/Autotest007/Program.cs
+++ Autotests/Autotest007/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Answer
+{
+    static void ShowCube(int N)
+    {
+        // Введите свое решение ниже
+    }
+
+    // Не удаляйте и не меняйте метод Main!
+    static public void Main(string[] args)
+    {
+        int N;
+
+        if (args.Length >= 1)
+        {
+            N = int.Parse(args[0]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 6;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
