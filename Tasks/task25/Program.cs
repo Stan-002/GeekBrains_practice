301ec5e251d9d909a437f183cf3a76a933058fb9
Barbossnya <Barbossnya@hotmail.com>
2023-10-16 16:37:56 +0300

    task25 resolved

--- Tasks/task25/Program.cs
+++ Tasks/task25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using System.Dynamic;
+using System;
+
+/*
+
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+*/
+
+System.Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int A, int B)
+{
+	int result = 1;
+	for (int i = 0; i < B; i++)
+	{
+		result *= A;
+	}
+	return result;
+}
+int result = Sum(A,B);
+System.Console.WriteLine(result);
