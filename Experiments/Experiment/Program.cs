ff2c0f4c7d6aa6ea3dc80a0a6a417bf2daf60abe
Barbossnya <Barbossnya@hotmail.com>
2023-10-19 09:50:17 +0300

    Change

--- Experiments/Experiment/Program.cs
+++ Experiments/Experiment/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System.Dynamic;
 using System;
 
-Console.Clear();
-System.Console.WriteLine("Введите имя пользователя: ");
-string user = Console.ReadLine();
+
+
+void Message(string user)
 {
-	if (user.ToLower() == "Аня")
+	if (user == "Аня")
 	{
 		System.Console.WriteLine("Привет, Аннечка Милая, как ты?");
 	}
-	else
+	else if (user != "Аня")
 	{
 		System.Console.WriteLine("О привет");
 	}
 }
+
+Console.Clear();
+System.Console.WriteLine("Введите имя пользователя: ");
+string user = Console.ReadLine();
+Message(user);
