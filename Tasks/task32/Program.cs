4a6ace89c11de894034340afa24d613f42787c9e
Barbossnya <Barbossnya@hotmail.com>
2023-10-19 15:10:18 +0300

    Change

---
+++ Tasks/task32/Program.cs
@@ -0,0 +1,59 @@
+﻿/* 
+
+32. Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, 
+и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+*/
+
+using System;
+Console.Clear();
+
+int[] GetRandomArray(int start, int end, int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] ChangePosAndNegArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i] * -1;
+    }
+    return result;
+}
+
+int TakeEntNum(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int userArraySize = TakeEntNum("Please, enter array size:");
+int userArrayStart = TakeEntNum("Please, enter array start number range:");
+int userArrayEnd = TakeEntNum("Please, enter array end number range:");
+
+int[] userArray = GetRandomArray(userArrayStart, userArrayEnd, userArraySize);
+int[] userChangedArray = ChangePosAndNegArray(userArray);
+PrintArray(userArray);
+PrintArray(userChangedArray);
---
+++ Tasks/task33/Program.cs
@@ -0,0 +1,125 @@
+﻿/* 
+
+33. Задайте массив. Напишите программу, 
+которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+
+*/
+
+using System;
+Console.Clear();
+
+int[] GetRandomArray(int start, int end, int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] ChangePosAndNegArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i] * -1;
+    }
+    return result;
+}
+
+int TakeEntNum(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool CheckNumberInArray(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+    return false;
+}
+
+int userArraySize = TakeEntNum("Please, enter array size:");
+int userArrayStart = TakeEntNum("Please, enter array start number range:");
+int userArrayEnd = TakeEntNum("Please, enter array end number range:");
+int userSearchNumber = TakeEntNum("Please, give number for search in array: ");
+
+int[] userArray = GetRandomArray(userArrayStart, userArrayEnd, userArraySize);
+
+PrintArray(userArray);
+
+System.Console.WriteLine(CheckNumberInArray(userArray, userSearchNumber));
+
+/* 
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+bool findNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        return true;
+    }
+    return false;
+}
+
+void PrintArray(int[] arrayOne, bool a)
+{
+    System.Console.WriteLine();
+    System.Console.Write("[");
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        System.Console.Write(arrayOne[i]);
+        if (i < arrayOne.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.Write("] -> ");
+    if (a == true) System.Console.WriteLine("Да");
+    else System.Console.WriteLine("Нет");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение для диапазона случайного числа: ");
+int max = Prompt("Конечное значене для дапазона случайного числа: ");
+int num = Prompt("Проверка числа в массиве: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array, findNum(array, num));
+
+*/
+
