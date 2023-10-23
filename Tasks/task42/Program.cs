2b935de56bf51bc4eb547c3ef898c4372832902b
Barbossnya <Barbossnya@hotmail.com>
2023-10-23 13:17:42 +0300

    Change

---
+++ Tasks/task42/Program.cs
@@ -0,0 +1,100 @@
+﻿/* 
+
+42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+
+binary - двоичное
+decimal - десятичное
+
+
+
+ */
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ToBinary( int number)
+{
+    int result= 0;
+    int ten = 1;
+    while(number>0){
+        result=number%2*ten+result;
+        number=number/2;
+        ten=ten*10;
+    }
+return result;
+}
+int Number = ToBinary(number);
+Console.Write(Number);
+
+/* 
+
+Console.Clear();
+50%
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Number(int binNumber)
+{
+    int bin = 0;
+    for (int i = binNumber; i > 0; i = i / 2)
+    {
+        if (i == 1)
+        {
+            bin = 1;
+        }
+        if (i % 2 == 0)
+        {
+            bin = 0;
+        }
+        else
+        {
+            bin = 1;
+        }
+        System.Console.Write(bin);
+    }
+}
+Number(number);
+
+
+ */
+
+ /* 
+ 100%
+ Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ToBinary( int number)
+{
+    int result= 0;
+    int ten = 1;
+    while(number>0){
+        result=number%2*ten+result;
+        number=number/2;
+        ten=ten*10;
+    }
+return result;
+}
+int Number = ToBinary(number);
+Console.Write(Number);
+  */
+
+  /* 
+  90%
+  string Conv(int number)
+{
+    string result = "";
+    int tmp = 0;
+    for (int i = number; i > 0; i/=2)
+    {
+        tmp = i % 2;
+        result = tmp + result;
+    }
+    return result;
+}
+
+int uNum = UserInOut("Введите число: ");
+System.Console.WriteLine(Conv(uNum));
+   */
---
+++ Tasks/task44/Program.cs
@@ -0,0 +1,99 @@
+﻿/* 
+
+44. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+
+*/
+
+//100% Решение Тима
+public class Program044
+{
+    public static void Main(string[] args)
+    {
+        int N = 7;
+        FibonacciNumbers(N);
+    }
+    private static void FibonacciNumbers(int number)
+    {
+        int[] arr = new int[number];
+        int firstElement = 0;
+        int secondElement = 1;
+        for (int i = 0; i < number; i++)
+        {
+            if (i < 2)
+            {
+                arr[i] = i;
+            }
+            else
+            {
+                arr[i] = arr[firstElement] + arr[secondElement];
+                firstElement++;
+                secondElement++;
+            }
+            System.Console.Write($"{arr[i]} ");
+        }
+    }
+}
+
+/* 
+
+//Элина Низамутдинова
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ToBinary( int number)
+{
+    int result= 0;
+    int ten = 1;
+    while(number>0){
+        result=number%2*ten+result;
+        number=number/2;
+        ten=ten*10;
+    }
+return result;
+}
+int Number = ToBinary(number);
+Console.Write(Number);
+
+*/
+
+/* 
+
+//80% 
+Console.Write("Введите количество чисел Фибоначчи: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 1)
+{
+    Console.WriteLine("Пожалуйста, введите положительное число.");
+    return;
+}
+
+int first = 0;
+int second = 1;
+
+Console.Write("Первые " + N + " чисел Фибоначчи: ");
+
+if (N >= 1)
+{
+    Console.Write(first);
+}
+
+if (N >= 2)
+{
+    Console.Write(", " + second);
+}
+
+for (int i = 3; i <= N; i++)
+{
+    int next = first + second;
+    Console.Write(", " + next);
+    first = second;
+    second = next;
+}
+
+Console.WriteLine();
+
+*/
