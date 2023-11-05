a19eb68cce62ad36658178635c3cea370417a5b6
Barbossnya <Barbossnya@hotmail.com>
2023-11-06 02:10:59 +0300

    Change

--- Experiments/Experiment01/Program.cs
+++ Experiments/Experiment01/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static int[,] IntroArray(int right, int left)
+    {
+        int[,] array = new int[right, left];
+        int result = 1;
+        
+        for (int i = 0; i < right; i++)
+        {
+            for (int j = 0; j < left; j++)
+            {
+                array[i, j] = result;
+                result++;
+            }
+        }
+        return array;
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Введите право: ");
+        int IntroRight = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Введите лево: ");
+        int IntroLeft = Convert.ToInt32(Console.ReadLine());
+        
+        int[,] user2DArray = IntroArray(IntroRight, IntroLeft);
+
+        // Выводим двумерный массив на консоль
+        for (int i = 0; i < IntroRight; i++)
+        {
+            for (int j = 0; j < IntroLeft; j++)
+            {
+                Console.Write(user2DArray[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
