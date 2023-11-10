b57091c1577202e62fe074a4712a5112174acc6a
Barbossnya <Barbossnya@hotmail.com>
2023-11-10 15:54:07 +0300

    Change

--- ExtraTasks/ExtraTask01/Program.cs
+++ ExtraTasks/ExtraTask01/Program.cs
@@ -7,3 +7,22 @@
 
  */
 
+Console.Clear();
+
+
+void EvenAndOdd(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            Console.WriteLine($"{numbers[i]} - это четное число");
+        }
+        else
+        {
+            Console.WriteLine($"{numbers[i]} - это нечетное число");
+        }
+    }
+}
+int[] numbers = { 5, 2, 7, 3, 4, 6, 8, 1, 9 };
+EvenAndOdd(numbers);
---
+++ ExtraTasks/ExtraTask02/Program.cs
@@ -0,0 +1,65 @@
+﻿/* 
+
+Задан генератором случайных чисел одномерный массив из действительных чисел. 
+Найдите максимальное и минимальное число этого массива.
+
+ */
+
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = CreateArray(10, 1, 100);
+
+        int min = array.Min();
+        int max = array.Max();
+
+        Console.WriteLine("Минимальное число массива: " + min);
+        Console.WriteLine("Максимальное число массива: " + max);
+    }
+
+    static int[] CreateArray(int size, int min, int max)
+    {
+        var random = new Random();
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        return array;
+    }
+}
+
+// int min = array.Min();
+// int max = array.Max();
+
+
+
+
+/* 
+
+void Output(int[] array)
+    {
+        Console.WriteLine("Введите количество элементов массива:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int min = array.Min();
+        int max = array.Max();
+
+        Console.WriteLine("Минимальное число массива: " + min);
+        Console.WriteLine("Максимальное число массива: " + max);
+    }
+ */
