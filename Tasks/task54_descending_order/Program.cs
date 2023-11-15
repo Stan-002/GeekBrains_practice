945d0b8b51d32373492559995a9626980ed85c03
Barbossnya <Barbossnya@hotmail.com>
2023-11-15 22:26:58 +0300

    Change

--- Tasks/task54_descending_order/Program.cs
+++ Tasks/task54_descending_order/Program.cs
@@ -5,3 +5,36 @@
 
  */
 
+
+using System;
+
+class Array2D
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите размер строки. i: ");
+        int i = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите размер столбца. j: ");
+        int j = Convert.ToInt32(Console.ReadLine());
+        int[][] arr1 = CreateAndFullArr(i, j, 0, 10);
+}
+    public static int[][] CreateAndFullArr(int row, int col, int startArray, int endArray)
+    {
+        int[][] array = new int[row][];
+        Random rd = new Random();
+        for (int i = 0; i < row; i++)
+        {
+            array[i] = new int[col];
+            Console.Write("[");
+            for (int j = 0; j < array[i].Length; j++)
+            {
+                array[i][j] = rd.Next(startArray, endArray);
+                Console.Write(" " + array[i][j]);
+            }
+            Console.WriteLine(" ]");
+
+
+        }
+        return array;
+    }
+}
--- Tasks/task56_find_the_string_with_the_smallest_sum_of_elements/Program.cs
+++ Tasks/task56_find_the_string_with_the_smallest_sum_of_elements/Program.cs
@@ -4,3 +4,91 @@
 программу, которая будет находить строку с наименьшей суммой элементов.
 
  */
+
+
+using System.Configuration.Assemblies;
+
+
+internal class Array2D
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер строки. i: ");
+        int low = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите размер столбца. j: ");
+        int high = Convert.ToInt32(Console.ReadLine());
+
+        int[][] arr1 = new int[low][];
+        int[][] arr2 = new int[high][];
+
+        
+        
+        QuickSort(arr1, 0, arr1.Length - 1);//Здесь
+        QuickSort(arr2, 0, arr2.Length - 1);
+
+
+
+        static void QuickSort(int[][] array, int low, int high)
+        {
+
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);//Здесь
+
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+
+            
+            static void Swap(int[][] array, int index1, int index2)
+            {
+                int[] temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+            static int Partition(int[][] array, int low, int high)
+            {
+                int[] pivot = array[high];
+                int i = low - 1;
+
+                for (int j = low; j < high; j++)
+                {
+                    if (CompareArrays(array[j], pivot) > 0)//Здесь
+                    {
+                        i++;
+                        Swap(array, i, j);
+                    }
+                }
+                Swap(array, i + 1, high);
+                return i + 1;
+            }
+            static int CompareArrays(int[] arr1, int[] arr2)
+            {
+                if (arr1.Length < arr2.Length)//Здесь
+                {
+                    return -1;
+                }
+                else if (arr1.Length > arr2.Length)
+                {
+                    return 1;
+                }
+                else
+                {
+                    for (int i = 0; i < arr1.Length; i++)
+                    {
+                        if (arr1[i] < arr2[i])
+                        {
+                            return -1;
+                        }
+                        else if (arr1[i] > arr2[i])
+                        {
+                            return 1;
+                        }
+                    }
+                    return 0;
+                }
+            }
+        }
+    }
+}
