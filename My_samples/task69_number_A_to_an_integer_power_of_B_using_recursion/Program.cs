fe0a45c94bf061b38a7eb3c956c0c4359510abe0
Barbossnya <Barbossnya@hotmail.com>
2023-11-21 07:27:56 +0300

    Change

---
+++ My_samples/task69_number_A_to_an_integer_power_of_B_using_recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+ */
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int result = PrintNumbers(A, B);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static int PrintNumbers(int A, int B)
+    {
+        if (B == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return A * PrintNumbers(A, B - 1);
+        }
+    }
+}
