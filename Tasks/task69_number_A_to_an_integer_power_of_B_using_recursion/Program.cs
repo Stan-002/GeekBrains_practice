cad0c78c874baa3c804d8599ce046eeb23d8d54b
Barbossnya <Barbossnya@hotmail.com>
2023-11-21 06:55:21 +0300

    Task 69 resolved

--- Tasks/task69_number_A_to_an_integer_power_of_B_using_recursion/Program.cs
+++ Tasks/task69_number_A_to_an_integer_power_of_B_using_recursion/Program.cs
@@ -4,4 +4,28 @@
 
  */
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
 
+        int result = PrintNumbers(A, B);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static int PrintNumbers(int A, int B)
+    {
+        if (B == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return A * PrintNumbers(A, B - 1);
+        }
+    }
+}
