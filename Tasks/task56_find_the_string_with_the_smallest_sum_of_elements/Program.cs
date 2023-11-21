32a895f40be8a3e07ba4d67599c3a31f6c8fcac4
Barbossnya <Barbossnya@hotmail.com>
2023-11-21 07:27:16 +0300

    Change

--- Tasks/task56_find_the_string_with_the_smallest_sum_of_elements/Program.cs
+++ Tasks/task56_find_the_string_with_the_smallest_sum_of_elements/Program.cs
@@ -6,7 +6,7 @@
  */
 
 
-using System.Configuration.Assemblies;
+
 
 
 //Недоделана
--- Tasks/task59_two-dimensional_array_of_integers/Program.cs
+++ Tasks/task59_two-dimensional_array_of_integers/Program.cs
@@ -18,190 +18,79 @@
  */
 
 
-//Недоделана
-using System;
 Console.Clear();
 
 
-int TakeEnteredNumber(string message)
-{
-    System.Console.WriteLine(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
-}
-
-
-int[,] GetRandom2DArray(int start, int end, int row, int column)
-{
-    int[,] array = new int[row, column];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            array[i, j] = new Random().Next(start, end + 1);
-        }
-    }
-    return array;
-}
-
-
 void Print2DArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            System.Console.Write(array[i, j] + "\t");
+            System.Console.Write($"{array[i, j]} \t");
         }
         System.Console.WriteLine();
     }
 }
-
-
-int[] FindMinIndex(int[,] array)
+int[] MinElement(int[,] array)
 {
-    int[] delete = {0,0};
-    int min =0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if(array[i,j] < min)
-            { 
-                delete[0] = i;
-                delete[1] = j;
-            }
-        }
-    }
-    return delete;
-}
-
-
-
-int[,] DeleteMinColumnRow (int[,] array, int row, int column)
-{
-int[,] result = new int [array.GetLength(0)- 1, array.GetLength(1) - 1] ;
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    if(i!= row)
-    {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        if(j != column)
-        {
-            result [i,j] = array [i,j];
-        }
-    }
-    }
-}
-return result;
-}
-
-int userArrayRow = TakeEnteredNumber("Введите количестов строк:");
-int userArrayColumn = TakeEnteredNumber("Введите количестов столбцов:");
-int userArrayStart = TakeEnteredNumber("Введите начало диапазона:");
-int userArrayEnd = TakeEnteredNumber("Введите конец диапазона:");
-int[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayRow, userArrayColumn);
-
-Print2DArray(user2DArray);
-int[] userMinIndex = FindMinIndex(user2DArray);
-int minRow = userMinIndex[0];
-int minColumn = userMinIndex[1];
-
-int[,] userDeletedArray = DeleteMinColumnRow(user2DArray, minRow, minColumn);
-System.Console.WriteLine();
-Print2DArray(userDeletedArray);
-
-/* 
-
-int UserEnter(string message)
-{
-    System.Console.Write(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
-}
-
-int[,] Get2DArray(int row, int column)
-{
-    int[,] array = new int[row, column];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            array[i, j] = new Random().Next(0, 10);
-        }
-    }
-    return array;
-}
-
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            System.Console.Write($"{array[i, j]}\t");
-        }
-        System.Console.WriteLine();
-    }
-}
-
-
-(int, int) FindMinIndex(int[,] array)
-{
-    int minRow = 0;
-    int minColumn = 0;
     int min = array[0, 0];
+    int[] arrInd = { 0, 0 };
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             if (array[i, j] < min)
             {
+
                 min = array[i, j];
-                minRow = i;
-                minColumn = j;
+                arrInd[0] = i; arrInd[1] = j;
             }
         }
     }
-    return (minRow, minColumn);
+    return arrInd;
 }
 
-int[,] DeleteMinColumnRow(int[,] array, int row, int column)
+int[,] DeletedRowsColumns(int[,] array)
 {
-    int[,] result = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
-    int x = 0;
+    int[] indArr = MinElement(array);
+    int minInRow = indArr[0];
+    int minInCol = indArr[1];
+    int[,] newArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+    int newInRow = 0;
+
 
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        if (i == row)
+        if (i !=minInRow )
         {
-            row = -1;
-            continue;
+            int newInCol = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (j != minInCol)
+                {
+                    newArray[newInRow, newInCol] = array[i, j];
+                    newInCol++;
+                }
+
+            }
+            newInRow++;
+            newInCol = 0;
         }
-        int y = 0;
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (j == column) continue;
-            result[x, y] = array[i, j];
-            y++;
-        }
-        x++;
     }
-    return result;
+    return newArray;
 }
 
+int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 },
+            { 5, 2, 6, 7 }
+        };
 
-
-
-
-int userRow = UserEnter("Ведите количество строк: ");
-int userColumn = UserEnter("Ведите количество столбцов: ");
-int[,] userArray = Get2DArray(userRow, userColumn);
-Print2DArray(userArray);
-(int minRow, int minColumn) = FindMinIndex(userArray);
-int[,] newArray = DeleteMinColumnRow(userArray, minRow, minColumn);
+Print2DArray(array);
 System.Console.WriteLine();
-Print2DArray(newArray);
 
- */
+System.Console.WriteLine();
+int[,] newArr = DeletedRowsColumns(array);
+Print2DArray(newArr);
